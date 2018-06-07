d03a851e78db671b3ac07ae4647e03dcf3f6c422
Lucas Coelho <lucascoelhosilvacs@gmail.com>
2018-06-07 20:18:35 -0300

    Adicionando Bcrypt e algumas plugins ao projeto

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,33 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.utils.SenhaUtils;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded: " + senhaEncoded);	
+			
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente: " + senhaEncoded);	
+
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));	
+		};
 	}
 }
