be9ccd976cff76b9a81f105ee10db2468ff483c3
Lucas Coelho <lucascoelhosilvacs@gmail.com>
2018-06-18 21:14:02 -0300

    adicionando a dependencia JPA no projeto

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,23 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import com.example.demo.utils.SenhaUtils;
+
+import com.example.demo.entities.Empresa;
+import com.example.demo.repositories.EmpresaRepository;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
 	
 	@Value("${paginacao.qtd_por_pagina}")
 	private int qtdPorPagina;
@@ -21,13 +29,36 @@ public class DemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
-			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Kazale IT");
+			empresa.setCnpj("74645215000104");
+			
+			this.empresaRepository.save(empresa);
+
+			List<Empresa> empresas = empresaRepository.findAll();
+			empresas.forEach(System.out::println);
+			
+			Empresa empresaDb = empresaRepository.findOne(1L);
+			System.out.println("Empresa por ID: " + empresaDb);
+			
+			empresaDb.setRazaoSocial("Kazale IT Web");
+			this.empresaRepository.save(empresaDb);
+
+			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+			
+			this.empresaRepository.delete(1L);
+			empresas = empresaRepository.findAll();
+			System.out.println("Empresas: " + empresas.size());
+			
+			/*String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
 			System.out.println("Senha encoded: " + senhaEncoded);	
 			
 			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
 			System.out.println("Senha encoded novamente: " + senhaEncoded);	
 
-			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));	
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));*/	
 		};
 	}
 }
