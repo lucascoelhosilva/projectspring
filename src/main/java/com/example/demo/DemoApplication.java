75b5779e22d87ca85519899900c58affceeefe16
Lucas Coelho <lucascoelhosilvacs@gmail.com>
2018-06-26 21:30:48 -0300

    adicionando exemplo ehcache

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,13 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
+import com.example.demo.services.ExemploService;
+
 @SpringBootApplication
+@EnableCaching
 public class DemoApplication {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ExemploService exemploService;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -24,7 +31,13 @@ public class DemoApplication {
     public CommandLineRunner commandLineRunner() {
         return args -> {
 
-            Usuario usuario = new Usuario();
+            System.out.println("Executando serviço pela primeira vez");
+            System.out.println(this.exemploService.exemploCache());
+
+            System.out.println("Executando serviço pela segunda vez, deve obter dados do cache");
+            System.out.println(this.exemploService.exemploCache());
+
+            /*Usuario usuario = new Usuario();
             usuario.setEmail("usuario@email.com");
             usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
             usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
@@ -34,7 +47,7 @@ public class DemoApplication {
             admin.setEmail("admin@email.com");
             admin.setPerfil(PerfilEnum.ROLE_ADMIN);
             admin.setSenha(SenhaUtils   .gerarBCrypt("123456"));
-            this.usuarioRepository.save(admin);
+            this.usuarioRepository.save(admin);*/
 
         };
     }
--- src/main/java/com/example/demo/services/ExemploService.java
+++ src/main/java/com/example/demo/services/ExemploService.java
@@ -1,11 +1,18 @@
 package com.example.demo.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExemploService {
-	
-	public void testarServico() {
-		System.out.println("### Executando serviço de teste!!");
+
+    private static final Logger log = LoggerFactory.getLogger(ExemploService.class);
+
+    @Cacheable("exemploCache")
+	public String exemploCache() {
+        log.info("#### Executando o serviço");
+        return "Teste de exemplo de cache";
 	}
 }
