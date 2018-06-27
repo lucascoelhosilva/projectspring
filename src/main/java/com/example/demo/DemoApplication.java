7fa4bd21ab58c50216c1fee1227d0deeb83a3908
Lucas Coelho <lucascoelhosilvacs@gmail.com>
2018-06-26 23:11:59 -0300

    adicionando o mongodb

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.documents.Cliente;
+import com.example.demo.repositories.ClienteRepository;
 import com.example.demo.security.entities.Usuario;
 import com.example.demo.security.repositories.UsuarioRepository;
 import com.example.demo.security.enums.PerfilEnum;
@@ -23,6 +25,9 @@ public class DemoApplication {
     @Autowired
     private ExemploService exemploService;
 
+    @Autowired
+    private ClienteRepository repository;
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
@@ -31,11 +36,31 @@ public class DemoApplication {
     public CommandLineRunner commandLineRunner() {
         return args -> {
 
-            System.out.println("Executando serviço pela primeira vez");
+            repository.deleteAll();
+
+            repository.save(new Cliente("Alice", 20));
+            repository.save(new Cliente("João", 30));
+            repository.save(new Cliente("Maria", 40));
+
+            System.out.println("Lista todos com o findAll():");
+            System.out.println("-------------------------------");
+            repository.findAll().forEach(System.out::println);
+            System.out.println();
+
+            System.out.println("Busca um único cliente com o findByName('Alice'):");
+            System.out.println("--------------------------------");
+            System.out.println(repository.findByNome("Alice"));
+            System.out.println();
+
+            System.out.println("Clientes com idade entre 18 and 35:");
+            System.out.println("--------------------------------");
+            repository.findByIdadeBetween(18, 35).forEach(System.out::println);
+
+            /*System.out.println("Executando serviço pela primeira vez");
             System.out.println(this.exemploService.exemploCache());
 
             System.out.println("Executando serviço pela segunda vez, deve obter dados do cache");
-            System.out.println(this.exemploService.exemploCache());
+            System.out.println(this.exemploService.exemploCache());*/
 
             /*Usuario usuario = new Usuario();
             usuario.setEmail("usuario@email.com");
--- src/main/java/com/example/demo/controllers/ExemploController.java
+++ src/main/java/com/example/demo/controllers/ExemploController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExemploController {
 
 	@GetMapping(value = "/{nome}")
-	@PreAuthorize("hasAnyRole('ADMIN')")
+//	@PreAuthorize("hasAnyRole('ADMIN')")
 	public String exemplo(@PathVariable("nome") String nome) {
 	    return "Olá " + nome;
 	}
